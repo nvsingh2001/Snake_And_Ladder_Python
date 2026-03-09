ec98c2bb55df5fe562967e0bf3245d28f12e0bef
nvsingh2001 <nvsingh2001@hotmail.com>
2026-03-09 15:29:15 +0530

    add: feature for 2 player to play the game

--- src/main.py
+++ src/main.py
@@ -8,32 +8,41 @@ def main():
 
     input("Press enter to start the game")
 
-    player1 = Player()
-
+    player1 = Player("Player 1")
+    player2 = Player("Player 2")
     count = 0
 
-    while player1.position < 100:
+    roll = 0
+
+    player = choice((player1, player2))
+
+    while player1.position < 100 and player2.position < 100:
+        count += 1
+
         options = ("NoPlay", "Ladder", "Snake")
 
         roll = Dice.roll()
-        count += 1
+
+        print(
+            f"Count: {count} Player: {player.name} rolled {roll} and is at {player.position}"
+        )
 
         match choice(options):
             case "NoPlay":
-                player1.position += 0
+                player = player2 if player == player1 else player1
             case "Ladder":
-                if player1.position + roll > 100:
+                if player.position + roll > 100:
                     continue
-                player1.position += roll
+                player.position += roll
             case "Snake":
-                player1.position -= roll
+                player.position -= roll
+                if player.position < 0:
+                    player.position = 0
 
-        if player1.position < 0:
-            player1.position = 0
+                player = player2 if player == player1 else player1
 
-        print(f"Count: {count} You rolled {roll} and moved to {player1.position}")
-
-    print("You Won!")
+    print(f"Count: {count} Player: {player.name} and is at {player.position}")
+    print(f"{player.name} won the game")
 
 
 if __name__ == "__main__":
--- src/player.py
+++ src/player.py
@@ -1,3 +1,4 @@
 class Player:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.position = 0
