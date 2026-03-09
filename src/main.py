701f0f305db9d38807d8fc7ac9ea81e842738bce
nvsingh2001 <nvsingh2001@hotmail.com>
2026-03-09 14:40:17 +0530

    add: completed loop to play the game with condition to repeat until player position is equal to 100

--- src/main.py
+++ src/main.py
@@ -4,19 +4,29 @@ from random import choice
 
 
 def main():
+    print("Welcome to snake and ladder game")
+
+    input("Press enter to start the game")
+
     player1 = Player()
 
-    options = ("NoPlay", "Ladder", "Snake")
+    while player1.position < 100:
+        if player1.position < 0:
+            player1.position = 0
 
-    roll = Dice.roll()
+        options = ("NoPlay", "Ladder", "Snake")
 
-    match choice(options):
-        case "NoPlay":
-            player1.position += 0
-        case "Ladder":
-            player1.position += roll
-        case "Snake":
-            player1.position -= roll
+        roll = Dice.roll()
+
+        match choice(options):
+            case "NoPlay":
+                player1.position += 0
+            case "Ladder":
+                player1.position += roll
+            case "Snake":
+                player1.position -= roll
+
+    print("You Won!")
 
 
 if __name__ == "__main__":
